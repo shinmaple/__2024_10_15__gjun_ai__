020f9fb915b0b0d03187f1ec1aa6e9dc8a9ecc3e
shinmaple <shinmaple@gmail.com>
2024-12-31 13:34:16 +0000

    lesson21_1.1

--- lesson21/main.py
+++ lesson21/main.py
@@ -1,11 +1,21 @@
 from flask import Flask
+import os
+
+import google.generativeai as genai
+
+from dotenv import load_dotenv
 
 app= Flask(__name__)
-
+genai.configure(api_key = os.environ['Gemini_API_KEY'])
+model = genai.GenerativeModel("gemini-1.5-flash")
 @app.route("/")
-@app.route("/<string:name>")
-def index(name:str=""):
-    return f"<p><h1>hello , happy new year! {name} !</h1></p>"
+@app.route("/<string:question>")
+def index(question:str=""):
+    response = model.generate_content(f"{question},回應輸出為html格式")
+    #return f"{question}"
+    html_format = response.text
+    html_format = html_format.replace("```html","").replace("```","")
+    return html_format
 
 @app.route('/hello')
 def hello():
